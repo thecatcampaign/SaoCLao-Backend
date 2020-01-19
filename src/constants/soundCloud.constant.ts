c146583b8972e42b67ddc34aeccc28a7a72c3a14
thecatcampaign <nguyentainguyen42pk@gmail.com>
2020-01-19 10:25:31 +0700

    refactor: hide SoundCloud client id in env file

--- src/constants/soundCloud.constant.ts
+++ src/constants/soundCloud.constant.ts
@@ -1,6 +1,5 @@
-export const CLIENT_ID = 'a281614d7f34dc30b665dfcaa3ed7505';
 export const soundCloundConfig = {
-  TRENDING_TRACKS_ENDPOINT: `https://api.soundcloud.com/tracks?linked_partitioning=1&limit=100&offset=0&client_id=${client_id}&tags=dubstep`,
-  SEARCH_TRACKS_ENDPOINT: `https://api.soundcloud.com/tracks?linked_partitioning=1&limit=20&offset=0&client_id=${client_id}&q=`,
+  TRENDING_TRACKS_ENDPOINT: `https://api.soundcloud.com/tracks?linked_partitioning=1&limit=100&offset=0&client_id=${process.env.CLIENT_ID}&tags=dubstep`,
+  SEARCH_TRACKS_ENDPOINT: `https://api.soundcloud.com/tracks?linked_partitioning=1&limit=20&offset=0&client_id=${process.env.CLIENT_ID}&q=`,
   GET_MEDIA_ENDPOINT: `https://api.soundcloud.com/tracks/`,
 };
